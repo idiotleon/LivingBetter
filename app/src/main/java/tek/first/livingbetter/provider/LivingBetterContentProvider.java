fda1c4c0845da52fd802131e88a44c02a96f12fe
leontheprofessional7 <yanglyu.leon.7@gmail.com>
2015-09-30 10:11:01 -0400

    ContentProvider class created

--- app/src/main/java/tek/first/livingbetter/provider/LivingBetterContentProvider.java
+++ app/src/main/java/tek/first/livingbetter/provider/LivingBetterContentProvider.java
@@ -1,7 +1,42 @@
 package tek.first.livingbetter.provider;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
 /**
  * Created by Leon on 9/30/2015.
  */
-public class LivingBetterContentProvider {
+public class LivingBetterContentProvider extends ContentProvider{
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
